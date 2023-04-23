0e0382ff8eb125764843504338b356bf0a191c29
IVAN KALENSKY <imitatehappiness@mail.ru>
2023-04-23 20:59:45 +0300

    Signed-off-by: IVAN KALENSKY <imitatehappiness@mail.ru>

---
+++ SearchAndReplaceData/main.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <chrono>
+
+#include "searchreplacedata.h"
+
+/*!
+\brief  Программа производит поиск и замену данных в шаблонах в произвольно вложенном каталоге файлов и папок. 
+        Для этого она парсит каталог, затем в каждом файле заменяет все вхождения заданных шаблонов на соответствующий текст замены. 
+        Программа работает в несколько потоков и сообщает пользователю о выполненной работе. 
+        Конфигурационный файл содержит информацию о количестве потоков, адресе корневой директории и перечислении шаблонов и их замен.
+*/
+int main() {
+    std::string configFilePath = "config.txt";
+
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    std::unique_ptr<SearchReplaceData> ptr(new SearchReplaceData(configFilePath));
+
+    if (!ptr->parseConfig()) {
+        std::cerr << "Unable to open config file: " << configFilePath << std::endl;
+        return -1;
+    }
+
+    ptr->parseDirectory();
+    std::cout << "Job is done." << std::endl;
+
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout << "time: " << duration.count() << " mc" << std::endl;
+
+    return 0;
+}
---
+++ SearchAndReplaceData/searchreplacedata.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+/*!
+ \brief Структура configFileData содержит данные, прочитанные из конфигурационного файла:
+ количество потоков numThreads, корневой каталог rootDir и список замен replacements,
+ который представляет собой вектор пар строк (старое значение и новое значение) для замены в файлах.
+*/
+struct ConfigFileData {
+    int numThreads;
+    std::string rootDir;
+    std::vector<std::pair<std::string, std::string>> replacements;
+};
+
+
+/*!
+\class SearchReplaceData
+\brief Класс SearchReplaceData осуществляет поиск и замену данных в шаблонах.
+*/
+class SearchReplaceData {
+public:
+    // Конструктор с параметрами
+    SearchReplaceData(const std::string& configFilePath);
+    // Деструктор
+    ~SearchReplaceData();
+    // Парсинг файла 
+    void parseFile(const std::string& path);
+    // Парсинг директории
+    void parseDirectory();
+    // Парсинг конфиг файла
+    bool parseConfig();
+private:
+    // Объект структуры, хранящий данные из конфиг файла
+    ConfigFileData* mConfigFileData;
+    // Путь до конфиг файла
+    std::string mConfigFilePath;
+};
+
+
